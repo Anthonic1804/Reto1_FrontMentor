0d5c1cce9e2e8d4b03bf22b634ddd272a25701d2
Antonio Hernández <54154901+Anthonic1804@users.noreply.github.com>
2023-10-22 22:03:34 -0600

    Completando el menu Movil

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,37 @@
 const hamburguerIcon = document.querySelector('.nav__hamburger');
+const navOverlay = document.querySelector('.nav__overlay');
 
 hamburguerIcon.addEventListener('click', ()=>{
     hamburguerIcon.classList.toggle('nav__hamburger--open');
+
+    navOverlay.classList.toggle('nav__overlay--show');
 });
+
+navOverlay.addEventListener('click', (e)=>{
+    e.preventDefault();
+    const currentElement = e.target;
+
+    //console.log(e.target.classList.value);
+
+    if(isActive(currentElement, 'nav__parent')){
+        //console.log('Tengo un subMenu');
+        const subMenu = currentElement.parentElement.children[1];
+        //console.log(subMenu);
+
+        if(window.innerWidth < 768){
+            
+            let height = (subMenu.clientHeight == 0)
+                        ? subMenu.scrollHeight : 0;
+
+            //console.log(subMenu.clientHeight);
+
+            subMenu.style.height = `${height}px`
+        }
+
+
+    }
+});
+
+function isActive(element, string){
+    return element.classList.value.includes(string);
+}
