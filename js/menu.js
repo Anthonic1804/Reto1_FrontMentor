d9ce366b51a39e1cd6ee4caa72da2268da91e429
Antonio Hernández <54154901+Anthonic1804@users.noreply.github.com>
2023-10-23 20:29:28 -0600

    Página terminada

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,6 @@
 const hamburguerIcon = document.querySelector('.nav__hamburger');
 const navOverlay = document.querySelector('.nav__overlay');
+let currentDropdown = navOverlay;
 
 hamburguerIcon.addEventListener('click', ()=>{
     hamburguerIcon.classList.toggle('nav__hamburger--open');
@@ -26,6 +27,12 @@ navOverlay.addEventListener('click', (e)=>{
             //console.log(subMenu.clientHeight);
 
             subMenu.style.height = `${height}px`
+        }else{
+            if(!isActive(subMenu, 'nav__inner--show')){
+                closeDropdown(currentDropdown);
+            }
+            subMenu.classList.toggle('nav__inner--show');
+            currentDropdown = subMenu;
         }
 
 
@@ -35,3 +42,24 @@ navOverlay.addEventListener('click', (e)=>{
 function isActive(element, string){
     return element.classList.value.includes(string);
 }
+
+function closeDropdown(currentDropdown){
+    if(isActive(currentDropdown, 'nav__inner--show')){
+        currentDropdown.classList.remove('nav__inner--show');
+    }
+}
+
+
+//FUNCION PARA QUE LOS ELEMENTO ABIERTOS DEL MENU SE
+//CIERREN AL MOMENTO DE CAMBIAR LA RESOLUCION DEL NAVEGADOR.
+window.addEventListener('resize', ()=>{
+    closeDropdown();
+    if(window.innerWidth > 768){
+        const navInners = document.querySelectorAll('.nav__inner');
+
+        navInners.forEach(navInner => {
+            navInner.style.height = '';
+        });
+    }
+});
+
